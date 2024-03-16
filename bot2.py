6a53605efa6f0debff3ac729cd5bacd829d83de8
Enoogy <91113824+Enoogy@users.noreply.github.com>
2024-03-16 16:49:48 +0500

    Add files via upload

--- bot2.py
+++ bot2.py
@@ -1,41 +1,88 @@
-import config
 import telebot
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+import config  # Importing the config module
 
-bot = telebot.TeleBot(config.token)
+rpc_connection = AuthServiceProxy(f'http://{config.rpc_user}:{config.rpc_password}@127.0.0.1:8276')
 
-# Подключение к RPC-серверу Bitcoin
-rpc_connection = AuthServiceProxy(config.rpc_url)
+bot_token = config.TOKEN  # Assuming you have config.TOKEN defined somewhere
+bot = telebot.TeleBot(bot_token)
 
-# Функция для генерации нового адреса
-def get_new_address():
-    try:
-        new_address = rpc_connection.getnewaddress()
-        return new_address
-    except JSONRPCException as e:
-        return f"Ошибка при генерации нового адреса: {e}"
-
-# Функция для получения баланса
-def get_balance():
-    try:
-        balance = rpc_connection.getbalance()
-        return balance
-    except JSONRPCException as e:
-        return f"Ошибка при получении баланса: {e}"
-
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Привет! Я бот. Как я могу помочь вам?")
+def address_balance(args):
+    inputs = rpc_connection.listunspent(0, 9999, args)
+    balance = 0
+    if len(inputs) == 0:
+        balance += 0
+    elif len(inputs) == 1:
+        balance += inputs[0].get("amount")
+    else:
+        for i in inputs:
+            balance += i.get("amount")
+    return balance
 
 @bot.message_handler(commands=['getnewaddress'])
-def send_new_address(message):
-    new_address = get_new_address()
-    bot.send_message(message.chat.id, f"Новый адрес: {new_address}")
+def get_new_address(message):
+    new_address = rpc_connection.getnewaddress()
+    bot.reply_to(message, f"New address: {new_address}")
 
 @bot.message_handler(commands=['getbalance'])
-def send_balance(message):
-    balance = get_balance()
-    bot.send_message(message.chat.id, f"Баланс кошелька: {balance} BTC")
+def get_balance(message):
+    balance = rpc_connection.getbalance()
+    bot.reply_to(message, f"Total wallet balance: {balance}")
+
+@bot.message_handler(commands=['send'])
+def send_coins(message):
+    global temp
+    args = message.text.split()[1:]
+    if len(args) != 3:
+        bot.reply_to(message, "Template: /send <sender_address> <receiver_address> <amount>")
+        return
+    sender_address, receiver_address, amount = args
+    try:
+        inputs = rpc_connection.listunspent(0, 9999, [sender_address])
+    except JSONRPCException:
+        bot.reply_to(message, f"Incorrect sender wallet address")
+        return
+
+    for i in inputs:
+        temp = i
+        if float(float(temp.get("amount"))) > (float(amount)+0.001):
+            break
+    if float(float(temp.get("amount"))) < (float(amount)+0.001):
+        bot.reply_to(message, "Insufficient funds")
+        return
+    fee = float(temp.get("amount")) - float(amount) - 0.001
+    inputForTransaction = {"txid":temp.get("txid"), "vout": temp.get("vout")}
+    try:
+        createTransaction = rpc_connection.createrawtransaction([inputForTransaction], {receiver_address:amount, sender_address:fee})
+    except JSONRPCException:
+        bot.reply_to(message, f"Incorrect receiver wallet address")
+        return
+    signTransaction = rpc_connection.signrawtransaction(createTransaction)
+    receivedHex = signTransaction.get("hex")
+    txid = rpc_connection.sendrawtransaction(receivedHex)
+    bot.reply_to(message, f"Coins sent to the receiver! Transaction ID: {txid}")
+
+@bot.message_handler(commands=['getaddressbalance'])
+def get_address_balance(message):
+    args = message.text.split()[1:]
+    if len(args) != 1:
+        bot.reply_to(message, "Template: /getaddressbalance <wallet_address>")
+        return
+    try:
+        balance = address_balance(args)
+    except JSONRPCException:
+        bot.reply_to(message, f"Incorrect wallet address ")
+        return
+    bot.reply_to(message, f"Address balance: {balance} KZC")
+
+@bot.message_handler(commands=['getalladdressesbalance'])
+def get_all_addresses_balance(message):
+    total_balance = get_total_balance()
+    bot.reply_to(message, f"Total balance of all addresses: {total_balance} KZC")
+
+@bot.message_handler(content_types=['text'])
+def send_message(message):
+    bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
     bot.infinity_polling()
--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 token = '7031917195:AAHakxx8bIokyzDLstVWjcI9sDzCKYfIsHo'
-rpc_url = "http://username:password@192.168.0.201:8277"
+rpc_user = 'kzcashrpc'
+rpc_password = 'tq3woSlOdymQWlVwDcDdYaUv'
